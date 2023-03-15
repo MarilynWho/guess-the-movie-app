460f1997601f8d3997403bf5b9cbe9ab69200795
MarilynWho <viktoriia.cheche@gmail.com>
2023-03-15 20:17:50 +0000

    Adding api call for OMDB and add random movie picker

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return <div data-test="component-home">
     <h1>Guess the movie</h1>
-      <button>Start</button>
+    <button onClick={() => navigate("/game")}>Start</button>
   </div>;
 };
 
