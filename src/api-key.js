f80a88516909419b6d2e3eed1be3d6a78e34b9fe
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-03-15 20:32:03 +0000

    - added style in homepage(not finish yet)

--- src/api-key.js
+++ src/api-key.js
@@ -1,3 +1,3 @@
-const Omdb_api = 8e9becb4;
+const Omdb_api = "8e9becb4";
 
-const dezzer = 4aaeaccd3934ccfd95af4a17bbf166c4;
+const dezzer = "4aaeaccd3934ccfd95af4a17bbf166c4";
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,23 @@
 import React from "react";
+import hand from "../images/Purple-in-Basic5.png";
 
 const Home = () => {
-  return <div data-test="component-home">
-    <h1>Guess the movie</h1>
-      <button>Start</button>
-  </div>;
+  return (
+    <div data-test="component-home" className="homePage">
+      <div className="Container">
+        <h1 className="title">Guess the movie</h1>
+      </div>
+      <div className="container home">
+        <div className="btn-one">
+          <button className="button">Start</button>
+          <div className="btn-two">
+            <button className="info">info</button>
+          </div>
+        </div>
+        <img src={hand} alt="Hand" className="hand"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
