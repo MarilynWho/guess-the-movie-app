b297fb858d5d6d4c95b3471af9591a7168f99c4e
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-03-15 16:37:27 +0000

    - added logo -style navbar - added classes to sytle navbar

--- src/components/Nvbar.js
+++ src/components/Nvbar.js
@@ -1,23 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import logo from "../images/logo.png";
 const Navbar = () => {
   return (
-    <div data-test="component-navbar">
-      <nav class="navbar sticky-top navbar-light bg-light">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
+    <nav className="navbar navbar-expand-lg ">
+      <img src={logo} alt="Guess the Movie" className="logo"></img>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="/navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarNav"
+      >
+        <ul className="navbar-nav">
+          <li className="nav-item mr-3">
+            <Link className="nav  " to="/">
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/scores">Scores</Link>
+          <li className="nav-item mr-3 ">
+            <Link className="nav" to="/scores">
+              Scores
+            </Link>
           </li>
-          <li>
-            <Link to="/info">Info</Link>
+          <li className="nav-item mr-3">
+            <Link className="nav " to="/info">
+              Info
+            </Link>
           </li>
         </ul>
-      </nav>
-    </div>
+      </div>
+    </nav>
+    //   <div data-test="component-navbar">
+    //     <nav className="navbar sticky-top">
+    //       <ul className="nav-link ">
+    //         <li>
+    //           <Link className="nav " to="/">
+    //             Home
+    //           </Link>
+    //         </li>
+    //         <li>
+    //           <Link className="nav " to="/scores">
+    //             Scores
+    //           </Link>
+    //         </li>
+    //         <li>
+    //           <Link className="nav " to="/info">
+    //             Info
+    //           </Link>
+    //         </li>
+    //       </ul>
+    //     </nav>
+    //   </div>
   );
 };
 
