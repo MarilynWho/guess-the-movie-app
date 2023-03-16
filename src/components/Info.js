19654ce682a5d2e5d5e3e7a0f413e1d926e6e002
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-03-16 15:42:38 +0000

    -added info -styled info title

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,7 +3,17 @@ import React from "react";
 const Info = (props) => {
   return (
     <div data-test="component-info">
-      <h1>Info</h1>
+      <h2 className="info-title">Info</h2>
+      <p>The concept is simple, can you guess that movie based on a song</p>
+      <p>
+        We use some exceptionally beatiful posters to give you a clue, and you
+        can play the song and check the corresponding song for the poster
+      </p>
+      <h5>How to play:</h5>
+      <p>- The player must answer 5 questions</p>
+
+      <h6>This game is for family and friends</h6>
+      <h6>Guess the movie App is made for movie-lovers</h6>
     </div>
   );
 };
