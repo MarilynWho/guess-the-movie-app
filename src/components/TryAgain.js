513a7754e0d5f23876a3030202d286c5a6604ea7
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-03-16 20:44:13 +0000

    - added scores button - added try again image and button

--- src/components/TryAgain.js
+++ src/components/TryAgain.js
@@ -1,7 +1,14 @@
 import React from "react";
+import handtwo from "../images/Purple-in-Jumper3.png";
 
 const TryAgain = (props) => {
-  return <div data-test="component-try-again"></div>;
+  return (
+    <div data-test="component-try-again">
+      <h1>Try Again</h1>
+      <button>Next</button>
+      <img src={handtwo} alt="hand" className="hand-two"></img>
+    </div>
+  );
 };
 
 export default TryAgain;
