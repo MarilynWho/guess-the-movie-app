ec4748283159b9d3ea062fe464fd8e148b645f5e
Darsing <catalunasiegfried@gmail.com>
2023-03-17 19:22:03 +0000

    added titles for Scores.js

--- src/components/Scores.js
+++ src/components/Scores.js
@@ -1,13 +1,43 @@
 import React from "react";
 
 const Scores = () => {
+  const bestScore = localStorage.getItem("newBestScore");
+
+  function whatTitle() {
+    let title;
+    if (bestScore <= 2) {
+      title = "Movie Geek";
+    } else if (bestScore === 3 || 4) {
+      title = "Film Nerd";
+    } else {
+      title = "Cinephile";
+    };
+    return title;
+  }
+
   return (
     <div data-test="component-scores">
-      <h2 className="score-title">My Score:</h2>
-      <h2 className="score-title">Your Best Score:</h2>
-      <button className="score-btn">Back Home</button>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col"></th>
+            <th scope="col">Your Title</th>
+            <th scope="col">Your Score</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"></th>
+            <td>{whatTitle()}</td>
+            <td>{bestScore}</td>
+          </tr>
+        </tbody>
+      </table>
+      <button className="button">Back Home</button>
     </div>
   );
 };
 
 export default Scores;
+
+
