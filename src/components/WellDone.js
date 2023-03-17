8ff6fe8af04c540a9a75815ed75645eb1d7534a0
Tatiana Leitao <tatiana_felisberto_@hotmail.com>
2023-03-17 19:56:51 +0000

    - added button and img

--- src/components/WellDone.js
+++ src/components/WellDone.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import handthree from "../images/Purple-in-Jumper3.png";
+import handthree from "../images/Purple-in-Jumper2.png";
 
 const WellDone = (props) => {
   const navigate = useNavigate();
@@ -8,17 +8,16 @@ const WellDone = (props) => {
   return (
     <div data-test="component-well-done">
       <h2 className="well-donetitle"> Well Done!!!</h2>
-      <button
-        className="button mb-3 justify-content-center"
-        onClick={() => navigate("/game")}
-      >
+
+      <button className="button mb-3 " onClick={() => navigate("/game")}>
         Another go
       </button>
       <button className="button" onClick={() => navigate("/score")}>
         I am tired
       </button>
+
       <div className="welldone-img">
-        <img scr={handthree} alt="hand"></img>
+        <img src={handthree} alt="hand"></img>
       </div>
     </div>
   );
